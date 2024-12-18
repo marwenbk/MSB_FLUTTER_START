0e8c1fc91e5b640db47a7da0f13f671fc472c767
marwen <marwanbk2000@gmail.com>
2024-12-18 11:29:05 +0100

    chore(dart): simplify more, removed theme

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -24,10 +24,6 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
